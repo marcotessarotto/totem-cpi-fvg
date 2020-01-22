b840d484684e2622d87dd5888ff2ed660824797a
arbihax <arbihax@gmail.com>
2020-01-22 16:30:21 +0100

    cambiato urls.py per non richiedere secondo livello

--- cpi_fvg/urls.py
+++ cpi_fvg/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-
+    path('', include('totem.urls')),
     path('totem/', include('totem.urls')),
     path('admin/', admin.site.urls),
 ]
