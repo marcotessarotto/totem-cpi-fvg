6a6e8611745fed379bbcf966c401640a06e59131
arbihax <arbihax@gmail.com>
2020-02-07 09:36:49 +0100

    moved script to timeout.js

---
+++ totem/static/totem/timeout.js
@@ -0,0 +1,35 @@
+//https://stackoverflow.com/questions/7071472/javascript-timeout-when-no-actions-from-user-for-specified-time-
+
+
+
+attachEvent(window,'load',function(){
+
+      var idleSeconds = 10;
+
+      var idleTimer;
+      function resetTimer(){
+        clearTimeout(idleTimer);
+        idleTimer = setTimeout(whenUserIdle,idleSeconds*1000);
+      }
+      attachEvent(document.body,'mousemove',resetTimer);
+      attachEvent(document.body,'keydown',resetTimer);
+      attachEvent(document.body,'click',resetTimer);
+
+      resetTimer(); // Start the timer when the page loads
+    });
+
+    function whenUserIdle(){
+
+      //window.location.href = "../../";
+      document.location.href="/";
+
+    }
+
+    function attachEvent(obj,evt,fnc,useCapture){
+      if (obj.addEventListener){
+        obj.addEventListener(evt,fnc,!!useCapture);
+        return true;
+      } else if (obj.attachEvent){
+        return obj.attachEvent("on"+evt,fnc);
+      }
+    }
